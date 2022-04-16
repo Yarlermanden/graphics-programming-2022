88355ce617871433a50be459843b4750ffd86035
yarlermanden <mmho@itu.dk>
2022-04-16 18:24:42 +0200

    First part of 10.1

--- exercises/exercise_10/main.cpp
+++ exercises/exercise_10/main.cpp
@@ -101,20 +101,20 @@ int main()
         lastFrame = currentFrame;
 
         // TODO 10.1 : Set sphere uniforms here, as material properties
-
-
+        defaultMaterial.SetPropertyValue<glm::vec3>("sphereCenter", s_Camera->ToViewSpace(sphereCenter, 1));
+        defaultMaterial.SetPropertyValue<glm::vec3>("sphereColor", sphereColor);
+        defaultMaterial.SetPropertyValue<float>("sphereRadius", sphereRadius);
 
 
         // TODO 10.1 : Material properties for the box, uncomment
-        //glm::mat4 boxMatrix(1.0f);
-        //boxMatrix = glm::translate(boxMatrix, boxCenter);
-        //boxMatrix = glm::rotate(boxMatrix, boxRotation.z, glm::vec3(0, 0, 1));
-        //boxMatrix = glm::rotate(boxMatrix, boxRotation.x, glm::vec3(1, 0, 0));
-        //boxMatrix = glm::rotate(boxMatrix, boxRotation.y, glm::vec3(0, 1, 0));
-        //defaultMaterial.SetPropertyValue<glm::mat4>("boxMatrix", s_Camera->GetViewMatrix() * boxMatrix);
-        //defaultMaterial.SetPropertyValue<glm::vec3>("boxColor", boxColor);
-        //defaultMaterial.SetPropertyValue<glm::vec3>("boxSize", boxSize);
-
+        glm::mat4 boxMatrix(1.0f);
+        boxMatrix = glm::translate(boxMatrix, boxCenter);
+        boxMatrix = glm::rotate(boxMatrix, boxRotation.z, glm::vec3(0, 0, 1));
+        boxMatrix = glm::rotate(boxMatrix, boxRotation.x, glm::vec3(1, 0, 0));
+        boxMatrix = glm::rotate(boxMatrix, boxRotation.y, glm::vec3(0, 1, 0));
+        defaultMaterial.SetPropertyValue<glm::mat4>("boxMatrix", s_Camera->GetViewMatrix() * boxMatrix);
+        defaultMaterial.SetPropertyValue<glm::vec3>("boxColor", boxColor);
+        defaultMaterial.SetPropertyValue<glm::vec3>("boxSize", boxSize);
 
         // TODO 10.2 : Set uniforms here, as material properties
 
@@ -273,8 +273,10 @@ void drawGui()
             ImGui::Separator();
 
             //TODO 10.1 : We are missing some properties!
-
-
+            ImGui::ColorEdit3("Box Color", (float*)&boxColor);
+            ImGui::DragFloat3("Box Center", (float*)&boxCenter, 0.0025f, -100, 100);
+            ImGui::DragFloat3("Box Size", (float*)&boxSize, 0.0025f, 0, 5);
+            //glm::vec3 boxSize(1.0f, 1.0f, 1.0f);
 
             ImGui::SliderAngle("Box Rotation X", (float*)&boxRotation.x, -180.0f, 180.0f);
             ImGui::SliderAngle("Box Rotation Y", (float*)&boxRotation.y, -180.0f, 180.0f);
@@ -283,7 +285,6 @@ void drawGui()
         }
 
         //TODO 10.2 : Add properties here
-
 
 
 
--- exercises/exercise_10/shaders/exercise10_1.frag
+++ exercises/exercise_10/shaders/exercise10_1.frag
@@ -1,6 +1,7 @@
 
 // Uniforms
 // TODO 10.1 : Replace constants with uniforms with the same name
+/*
 const vec3 sphereColor = vec3(0, 0, 1);
 const vec3 sphereCenter = vec3(-2, 0, -10);
 const float sphereRadius = 1.25f;
@@ -8,6 +9,14 @@ const float sphereRadius = 1.25f;
 const vec3 boxColor = vec3(1, 0, 0);
 const mat4 boxMatrix = mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 2,0,-10,1);
 const vec3 boxSize = vec3(1, 1, 1);
+*/
+uniform vec3 sphereColor;
+uniform vec3 sphereCenter;
+uniform float sphereRadius;
+
+uniform vec3 boxColor;
+uniform mat4 boxMatrix;
+uniform vec3 boxSize;
 
 // Configure ray marcher
 void GetRayMarcherConfig(out int maxSteps, out float maxDistance, out float surfaceDistance)
